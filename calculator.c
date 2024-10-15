ad215c2638bc050475a45642ff894dc1b338e297
ved prakash singh <141497727+antiquark007@users.noreply.github.com>
2024-10-15 17:16:13 +0530

    to local by dev2

--- calculator.c
+++ calculator.c
@@ -5,4 +5,17 @@ int main(){
     scanf("%d",&a);
     printf("Enter the  second number: ");
     scanf("%d",&b);
+    printf("Enter operator  (+,-,*,/): ");
+    char op;
+    scanf(" %c",&op);
+    switch (op)
+    {
+    case '+':
+        printf("%d+%d=%d",a,b,a+b);
+        break;
+    
+    default:
+        break;
+    }
+
 }
