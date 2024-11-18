fc31d31707484c6aa87a64aa2fbebd183c6357f9
antiquark <141497727+antiquark007@users.noreply.github.com>
2024-11-18 06:41:14 +0530

    b2

--- file.c
+++ file.c
@@ -1,1 +1,2 @@
-this is new file file.c created by vi file.c after pressing letter i and esc key word we can write in  it wq save and quit w save q! quit without saving
+// this is new file file.c created by vi file.c after pressing letter i and esc key word we can write in  it wq save and quit w save q! quit without saving
+// new changes are done for file.c by branch b2
